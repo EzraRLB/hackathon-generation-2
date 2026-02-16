522f3060b15070e5059c9b73492795e1604aecd0
EzraRLB <israelrivera2001@hotmail.com>
2026-02-16 10:30:11 -0600

    Added instructions for Hackathon evaluation criteria

--- src/org/generation/models/Agenda.java
+++ src/org/generation/models/Agenda.java
@@ -1,4 +1,39 @@
 package org.generation.models;
 
+// Podría extender alguna estructura de datos como ArrayList, Array, etc.
 public class Agenda {
+
+    // Recibe contactos como objetos.
+
+
+    // Se crea de dos formas: Indicándole nosotros el tamaño:
+
+
+    // O con un tamaño por defecto:
+
+
+    // añadirContacto: Añade un contacto a la agenda, si no se pueden
+    // añadir más a la agenda se indicará por pantalla indicando el motivo.
+
+
+    // existeContacto: Indica si un contacto existe en la agenda.
+
+
+    // listaContactos: Muestra los contactos de la agenda.
+
+
+    // buscarContacto: Busca un contacto por su nombre y muestra su teléfono.
+
+
+    // eliminarContacto: Elimina un contacto de la agenda. Indica si se ha eliminado o no por pantalla.
+
+    // agendaLlena: Indica si la agenda está llena. Debería mandarse a llamar cuando
+    // se quiera añadir un contacto y
+    // no haya espacio disponible.
+
+
+    // espacioDisponible: Indica el espacio disponible en la agenda. Cuenta los contactos
+    // que conforman la agenda y lo resta al tamaño total de la agenda.
+
+
 }
--- src/org/generation/models/Contact.java
+++ src/org/generation/models/Contact.java
@@ -1,4 +1,9 @@
 package org.generation.models;
 
 public class Contact {
+    // Nombre.
+
+    // Teléfono.
+
+
 }
