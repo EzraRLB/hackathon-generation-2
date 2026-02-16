1b42f192ab86c0d58a4e54529ab0aec94d51f8e0
EzraRLB <israelrivera2001@hotmail.com>
2026-02-16 10:21:38 -0600

    Added Contact and Agenda classes

---
+++ src/org/generation/exceptions/InvalidDataInput.java
@@ -0,0 +1,7 @@
+package org.generation.exceptions;
+
+public class InvalidDataInput extends RuntimeException {
+    public InvalidDataInput(String message) {
+        super(message);
+    }
+}
---
+++ src/org/generation/models/Contact.java
@@ -0,0 +1,4 @@
+package org.generation.models;
+
+public class Contact {
+}
